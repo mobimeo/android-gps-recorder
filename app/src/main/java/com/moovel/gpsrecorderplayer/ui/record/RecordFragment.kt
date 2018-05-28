4bd050207077cbcd8c3f2b9234b4c6a4bd734ec1
Johannes Lauber <johannes.lauber@moovel.com>
2018-05-28 14:10:05 +0200

    Edit record name

--- app/src/main/java/com/moovel/gpsrecorderplayer/ui/record/RecordFragment.kt
+++ app/src/main/java/com/moovel/gpsrecorderplayer/ui/record/RecordFragment.kt
@@ -3,16 +3,16 @@ package com.moovel.gpsrecorderplayer.ui.record
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.moovel.gpsrecorderplayer.R
+import kotlinx.android.synthetic.main.record_fragment.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter.ISO_DATE
 
 class RecordFragment : Fragment(), OnMapReadyCallback {
     private lateinit var viewModel: RecordViewModel
@@ -31,6 +31,9 @@ class RecordFragment : Fragment(), OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        edit_record_name.setText(getString(R.string.record_new_record, LocalDate.now().format(ISO_DATE), 1))
+        edit_record_name.requestFocus()
+        edit_record_name.setSelection(edit_record_name.text.length)
     }
 
     override fun onMapReady(p0: GoogleMap?) {
@@ -40,10 +43,5 @@ class RecordFragment : Fragment(), OnMapReadyCallback {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(RecordViewModel::class.java)
         // TODO: Use the ViewModel
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
-        inflater?.inflate(R.menu.edit, menu)
-        super.onCreateOptionsMenu(menu, inflater)
     }
 }
