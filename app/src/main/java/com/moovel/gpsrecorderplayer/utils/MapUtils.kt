0c899b87422ce89ea4df37f9c6072a44289774b1
Johannes Lauber <johannes.lauber@moovel.com>
2018-07-31 10:19:14 +0200

    fix carsh on small screens

--- app/src/main/java/com/moovel/gpsrecorderplayer/utils/MapUtils.kt
+++ app/src/main/java/com/moovel/gpsrecorderplayer/utils/MapUtils.kt
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.LocationSource
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
+import java.lang.Exception
 
 fun GoogleMap.setLocationSource(source: LiveData<Location>) {
     setLocationSource(object : LocationSource, Observer<Location> {
@@ -37,5 +38,9 @@ operator fun LatLng.component2() = longitude
 fun GoogleMap.zoomToPolyline(polyline: List<LatLng>) {
     val boundsBuilder = LatLngBounds.Builder()
     polyline.forEach { boundsBuilder.include(it) }
-    moveCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(), 24.dpToPx()))
+    try {
+        moveCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(), 24.dpToPx()))
+    } catch (e: Exception) {
+        // can be ignored. Screen is too small to show the map
+    }
 }
