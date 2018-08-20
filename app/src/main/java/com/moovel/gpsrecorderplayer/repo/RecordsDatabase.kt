2c8d12925def548af942a5393b31b97fbe660183
Johannes Lauber <johannes.lauber@moovel.com>
2018-08-20 15:10:02 +0200

    add license to kotlin files

--- app/src/main/java/com/moovel/gpsrecorderplayer/repo/RecordsDatabase.kt
+++ app/src/main/java/com/moovel/gpsrecorderplayer/repo/RecordsDatabase.kt
@@ -1,3 +1,25 @@
+/**
+ * Copyright (c) 2010-2018 Moovel Group GmbH - moovel.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.moovel.gpsrecorderplayer.repo
 
 import android.content.Context
@@ -19,6 +41,8 @@ internal abstract class RecordsDatabase : RoomDatabase() {
     abstract fun signalsDao(): SignalsDao
 
     companion object {
+        private var instance: RecordsDatabase? = null
+
         private val MIGRATION_1_2 = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) = database.transaction {
                 execSQL("ALTER TABLE records ADD COLUMN created INTEGER NOT NULL DEFAULT 0")
@@ -37,7 +61,6 @@ internal abstract class RecordsDatabase : RoomDatabase() {
             }
         }
 
-        private var instance: RecordsDatabase? = null
         fun getInstance(context: Context): RecordsDatabase {
             if (instance != null) return instance!!
             synchronized(Companion) {
